51c5d9bfee47254a4c1390f9b6a12b11e0fe068e
fk <fk>
2007-06-19 16:38:45 +0000

    Wed, 17 Jan 2007 03:47:55 +0100:   * Changed maintainer.   * Adds explicit cron mode (--cron-mode).   * Handles de-escalition of notification more accurately.

--- debian/cron.d
+++ debian/cron.d
@@ -4,5 +4,5 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-*/5 *   * * *   root    /usr/sbin/find-idle-root-shells
+*/5 *   * * *   root    /usr/sbin/find-idle-root-shells --cron-mode
 
