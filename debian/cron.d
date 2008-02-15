49122194a0f32b3acfc2770e0111e980665f8a12
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2008-02-15 01:22:55 +0100

    Removing Debian package information from master branch.

--- debian/cron.d
+++
@@ -1,8 +0,0 @@
-#
-# Regular cron jobs for the find-idle-root-shells package
-#
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-*/5 *   * * *   root    /usr/sbin/find-idle-root-shells --cron-mode
-
