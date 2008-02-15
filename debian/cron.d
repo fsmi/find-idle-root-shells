6a8fadbfec3b2b5530d82b65873c62114c5ec879
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2008-02-15 02:03:06 +0100

    Check for executable existance in crontab rule.

--- debian/cron.d
+++ debian/cron.d
@@ -4,5 +4,5 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-*/5 *   * * *   root    /usr/sbin/find-idle-root-shells --cron-mode
+*/5 *   * * *   root    [ -x /usr/sbin/find-idle-root-shells ] && /usr/sbin/find-idle-root-shells --cron-mode
 
